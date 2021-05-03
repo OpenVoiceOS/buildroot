fe22d4007afd3a673fa935d02fda6ff1b84ef274
Christian Stewart <christian@paral.in>
2021-05-02 19:08:54 -0700

    package/libfuse3: allow building as a host package
    Adds 1 patch to fix an install error when building as a host package.
    
    Patch was submitted upstream:
    https://github.com/libfuse/libfuse/pull/598
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    
    ---
    
    v1 -> v2:
    
     - adjusted patch to completely remove init.d script install
    
    TODO for V3:
    
    https://patchwork.ozlabs.org/project/buildroot/patch/20210516010521.1375959-1-christian@paral.in/
    
     - add init.d scripts for openrc and systemd

--- package/libfuse3/libfuse3.mk
+++ package/libfuse3/libfuse3.mk
@@ -12,11 +12,17 @@ LIBFUSE3_CPE_ID_VENDOR = libfuse_project
 LIBFUSE3_CPE_ID_PRODUCT = libfuse
 LIBFUSE3_INSTALL_STAGING = YES
 LIBFUSE3_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+
 LIBFUSE3_CONF_OPTS = \
 	-Dexamples=false \
 	-Dudevrulesdir=/lib/udev/rules.d \
 	-Duseroot=false \
 	-Dtests=false
+
+HOST_LIBFUSE3_CONF_OPTS = \
+        -Dexamples=false \
+        -Dudevrulesdir=$(HOST_DIR)/lib/udev/rules.d \
+        -Duseroot=false
 
 # Uses __atomic_fetch_sub_4
 ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
@@ -36,3 +42,4 @@ define LIBFUSE3_LINUX_CONFIG_FIXUPS
 endef
 
 $(eval $(meson-package))
+$(eval $(host-meson-package))
