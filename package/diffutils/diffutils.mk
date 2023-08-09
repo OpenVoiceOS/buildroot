d281771f2cf64a6c7878011fdc95601ac9c33496
Clément Ramirez <clement.ramirez@bootlin.com>
2023-08-09 08:15:01 +0200

    package/diffutils: add DIFFUTILS_CPE_ID_VENDOR
    cpe:2.3:a:gnu:diffutils is a valid identifier for
    this package:
    https://nvd.nist.gov/products/cpe/search/results?namingFormat=2.3&keyword=diffutils
    
    Signed-off-by: Clément Ramirez <clement.ramirez@bootlin.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit e763128b6b16becdb476f52af106302e18c26233)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/diffutils/diffutils.mk
+++ package/diffutils/diffutils.mk
@@ -13,6 +13,9 @@ DIFFUTILS_LICENSE_FILES = COPYING
 DIFFUTILS_CPE_ID_VENDOR = gnu
 DIFFUTILS_CONF_ENV += gl_cv_func_strcasecmp_works=yes
 
+# We're patching m4/stack-direction.m4
+DIFFUTILS_AUTORECONF = YES
+
 # Since glibc >= 2.26, don't try to use getopt_long replacement bundled
 # with diffutils. It will conflict with the one from glibc.
 ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
